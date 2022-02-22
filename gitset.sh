d93f4caeb88c122f1d80fa1ee933e99fbc72022a
starlight <starlightcommsng@gmail.com>
2022-02-22 23:28:25 +0100

    add exe and sh

---
+++ gitset.sh
@@ -0,0 +1,7 @@
+echo "# zelle_team_repo" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/StarlightZelle/zelle_team_repo.git
+git push -u origin main
